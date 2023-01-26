dba2a19b0831f6019d37bcb09ccc210626b7b1d6
acastineiraduran <acastineiraduran@danielcastelao.org>
2023-01-26 23:53:48 +0100

    A

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("primera mod en master");
+        System.out.println("segunda mod en master");
+    }
+
+}
